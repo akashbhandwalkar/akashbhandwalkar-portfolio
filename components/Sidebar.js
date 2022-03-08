858ce2d42eb746aa630d765fc38d2516a5ade83e
Akash Bhandwalkar <abakash02@gmail.com>
2022-03-08 15:18:36 +0530

    Added layout

---
+++ components/Sidebar.js
@@ -0,0 +1,8 @@
+export default function Sidebar({ title }) {
+    return (
+      <div className="sidebar-wrapper">
+
+      </div>
+    )
+  }
+  
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '@styles/globals.css'
-
+import '@styles/globals.scss'
+import Header from '@components/Header';
+import Sidebar from '@components/Sidebar';
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Header></Header>
+    <Sidebar />
+    <Component {...pageProps} />
+  </>
+  
+  )
 }
 
 export default Application
