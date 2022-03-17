00c707fbb3f991d0f85f0d979b8ddb0f4db5d0fc
Akash Bhandwalkar <abakash02@gmail.com>
2022-03-17 21:13:00 +0530

    Created sidebar

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 export default function Header({ title }) {
   return(<div className="header-wrapper">
-    
+    <span> akashbhandwalkar.in | Full Stack Web Developer</span>
   </div>)
 }
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,23 @@
 export default function Sidebar({ title }) {
     return (
       <div className="sidebar-wrapper">
+          <div className="profile-pic-wrapper">
+          </div>
+          <div>
+            <h2 className="name">Akash Bhandwalkar</h2>
+          </div>
+          <div className="position">
+            Software Engineer at Checkpoint | React - Redux | AngularJS | HTML | CSS | JS | docker | aws
+          </div>
 
-      </div>
+          <ul className="navbar">
+            <li className="nav-item"> Articles </li>
+            <li className="nav-item"> Projects </li>
+            <li className="nav-item"> Games </li>
+            <li className="nav-item"> CSS Art </li>
+            <li className="nav-item"> Drafts / Learnings </li>
+          </ul>
+        </div>
     )
   }
   
